82884e575f86c18b7e5f9733eab82f011b1e0c4d
Underemployed <underemployed871@gmail.com>
2025-03-12 22:18:27 +0530

    modular component feature

--- js/main.js
+++ js/main.js
@@ -124,3 +124,25 @@
     
 })(jQuery);
 
+
+
+// Load useless HTML files
+loadHTML('navbar', 'html/nav.html');
+
+
+
+const loadHTML = (elementId, filePath) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', filePath, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            document.getElementById(elementId).innerHTML = xhr.responseText;
+        } else {
+            console.error(`Error loading ${filePath}:`, xhr.status, xhr.statusText);
+        }
+    };
+    xhr.onerror = function () {
+        console.error(`Request for ${filePath} failed`);
+    };
+    xhr.send();
+}
