08146ffe80e73919b11400475a2f744b5a49d477
Underemployed <underemployed871@gmail.com>
2025-06-25 13:53:08 +0530

    contact mailing system done

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,9 @@
 $("#client-form").submit(function (e) {
     e.preventDefault();
+
+    $(".send-div").removeClass("error");
+    $(".send-div").removeClass("success");
+
     $(".send-div").show();
     $(".loader-text").html("Sending...<br>Please wait...");
     const formData = $(this).serializeArray();
@@ -13,7 +17,9 @@ $("#client-form").submit(function (e) {
 
     $.ajax({
         url: `
+
 https://script.google.com/macros/s/AKfycbxJlv1NmFlwGK6TAzSaIe1etXk6FUJlU6vw6pvBSOu25oDbYEmUPDJ6P0JBL32C1L6W/exec
+
 
         `.trim(),
         method: "POST",
@@ -21,13 +27,14 @@ https://script.google.com/macros/s/AKfycbxJlv1NmFlwGK6TAzSaIe1etXk6FUJlU6vw6pvBS
         success: function (response) {
             // console.log("Success:", response);
             $(".loader-text").html("Message sent successfully");
-            $(".send-div").fadeOut(1500);
+            $(".send-div").addClass("success");
+            $(".send-div").fadeOut(2000);
         },
         error: function (err) {
             $(".loader-text").html("An error occurred while sending the message.<br>Please try again later.");
-            $(".send-div").fadeOut(1500);
-        
-           
+            $(".send-div").fadeOut(2000);
+            $(".send-div").addClass("error");
+
         }
     });
 });
